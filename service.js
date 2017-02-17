9b1189d012758da98a9e8043618dc5ff0afc7fb2
Richard Wurth <Richard Wurth>
2017-02-16 19:31:40 -0700

    Initial submission, missing injected code from slack

--- service.js
+++ service.js
@@ -1,10 +1,54 @@
-angular.module('userProfiles').service('mainService', function($http) {
+angular.module('userProfiles').service('mainService', function($http, $q) {
+
 
   this.getUsers = function() {
-    return $http({
-        method: 'GET',
-        url: 'http://reqres.in/api/users?page=1'
-    })
-  }
+    const deferred = $q.defer();
 
+    let allUsers = [];
+    const resolutions = {};
+
+      $http.get('https://reqres.in/api/users?page=' + pageNum)
+      .then(function(response){
+        allUsers = allUsers.concat(response.data.data);
+        resolutions[i] = true;
+        const every = Object.keys(resolutions).every(function(resNum){return resolutions[resNum]});
+        if (every) {
+          deferred.resolve(allUsers);
+        }
+      });
+    }
+
+      for (var i = 1; i < 3; i++) {
+        resolutions[i] = false;
+        makeRequest(i);
+      }
+
+    return deferred.promise;
+
+  }
 });
+
+
+
+//     return $http.get('https://reqres.in/api/users?page=1')
+//       .then(function(responseOne){
+//         $http.get('https://reqres.in/api/users?page=2')
+//           .then(function(responseTwo){
+//             $http.get('https://reqres.in/api/users?page=3')
+//               .then(function(responseThree){
+//                 return responseOne.data.data.concat(responseTwo.data.data, responseThree.data.data);
+//               })
+//           })
+//       });
+// const promisesList = [
+//   $http.get('https://reqres.in/api/users?page=1'),
+//   $http.get('https://reqres.in/api/users?page=2'),
+//   $http.get('https://reqres.in/api/users?page=3'),
+// ];
+//
+// return $q.all(promisesList)
+// .then(function(responsesArr){
+//   return responsesArr.reduce(function(flattenedUsersArr, responseObj){
+//     return flattenedUsersArr.concat(responseObj.data.data);
+//   }, []);
+// });
